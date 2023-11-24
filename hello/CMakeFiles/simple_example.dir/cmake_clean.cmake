31db09e90cd94d7b7f4f278fb5cbe90885399b62
Surendrakumar <techneo@users.noreply.github.com>
2023-11-24 05:36:28 +0000

    -a

---
+++ hello/CMakeFiles/simple_example.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple_example.dir/hello.cpp.o"
+  "simple_example"
+  "simple_example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simple_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
